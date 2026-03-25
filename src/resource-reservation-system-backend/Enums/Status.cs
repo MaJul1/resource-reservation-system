3f09567a796e7d6dcd7cc542a8c08154cbc60763
majul <majul@example.com>
2026-03-25 09:33:02 +0800

    Add Status enum and Reservation, Resource, User models; update project file structure

---
+++ src/resource-reservation-system-backend/Enums/Status.cs
@@ -0,0 +1,10 @@
+namespace resource_reservation_system_backend.Enums;
+
+public enum Status
+{
+  APPROVED,
+  DENIED,
+  CANCELLED,
+  ONGOING,
+  MOVED
+}
---
+++ src/resource-reservation-system-backend/Models/Reservation.cs
@@ -0,0 +1,12 @@
+using System;
+using resource_reservation_system_backend.Enums;
+
+namespace resource_reservation_system_backend.Models;
+
+public class Reservation
+{
+  public int Id {get; set;}
+  public DateTime Start {get; set;}
+  public DateTime End {get; set;}
+  public Status Status {get; set;}
+}
